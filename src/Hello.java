31ae431a03157136782aee97c87cfa1d443ada04
Ragini1995 <raginidahihande@rediffmail.com>
2015-01-29 15:38:12 -0800

    all files are added

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Ragini19 on 29/01/2015.
+ */
+public class Hello
+{
+    public static void main()
+    {
+        System.out.println("Hello");
+    }
+}
---
+++ src/Welcome.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Ragini19 on 29/01/2015.
+ */
+public class Welcome {
+    public static void main()
+    {
+        System.out.println("Welcome");
+    }
+}
